1976de27cb54032edcc0ec3d97ea479dfc676e60
zoziha13 <1325686572@qq.com>
2021-07-26 12:50:28 +0800

    Add hybrd interface and example.

--- src/minpack.f90
+++ src/minpack.f90
@@ -12,6 +12,26 @@ interface
     double precision x(n)
     end function
 
+    !> The purpose of `hybrd` is to find a zero of a system of N non-
+    !>  linear functions in N variables by a modification of the Powell
+    !>  hybrid method.  The user must provide a subroutine which calcu-
+    !>  lates the functions.  The Jacobian is then calculated by a for-
+    !>  ward-difference approximation.
+    subroutine hybrd(fcn,n,x,fvec,xtol,maxfev,ml,mu,epsfcn,diag, &
+                         mode,factor,nprint,info,nfev,fjac,ldfjac, &
+                         r,lr,qtf,wa1,wa2,wa3,wa4)
+        integer n,maxfev,ml,mu,mode,nprint,info,nfev,ldfjac,lr
+        double precision xtol,epsfcn,factor
+        double precision x(n),fvec(n),diag(n),fjac(ldfjac,n),r(lr),qtf(n), &
+                         wa1(n),wa2(n),wa3(n),wa4(n)
+        interface
+            subroutine fcn(n,x,fvec,iflag)
+                integer n,iflag
+                double precision x(n),fvec(n)
+            end subroutine fcn
+        end interface
+    end subroutine hybrd
+
     !> The purpose of `hybrd1` is to find a zero of a system of
     !>  n nonlinear functions in n variables by a modification
     !>  of the powell hybrid method.
